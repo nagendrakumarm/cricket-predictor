920cc2552e3d06f885f91125ae539b1db8fef77f
nagendrakumarm <nagendrakumarm@gmail.com>
2026-05-10 23:52:43 -0400

    updated environment url

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private base = 'http://localhost:8000'; // your backend
+  private base = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
